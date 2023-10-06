f9346ec780afca6553b42bb7ebfb7a910b354167
Ars-Lupin <dcluizfellps@hotmail.com>
2023-10-06 17:14:35 -0300

    biblioteca

---
+++ pacman/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_PATH_SIZE 1001 // Tamanho maximo do diretorio
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) // caso o diretorio nao seja informado
+    {
+        printf("ERRO: O diretorio de arquivos de configuracao nao foi informado\n");
+        return 1;
+    }
+
+    char diretorio[MAX_PATH_SIZE];
+    strcpy(diretorio, argv[1]);
+
+    return 0;
+}
